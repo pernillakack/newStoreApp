ee7509cc5691a87742e31ca3f886b13735980e74
PhilNordin <112933450+PhilNordin@users.noreply.github.com>
2023-12-04 13:20:25 +0100

    firebaseConfig fix

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,21 +2,14 @@ import { initializeApp } from "firebase/app"
 // import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
-const key = process.env.EXPO_PUBLIC_FIREBASE_KEY
-const aDomain = process.env.EXPO_PUBLIC_AUTHDOMAIN
-const pId = process.env.EXPO_PUBLIC_PROJECTID
-const storageB = process.env.EXPO_PUBLIC_STORAGEBUCKET
-const sendId = process.env.EXPO_PUBLIC_SENDERID
-const appId = process.env.EXPO_PUBLIC_APPID
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: key,
-  authDomain: aDomain,
-  projectId: pId,
-  storageBucket:storageB,
-  messagingSenderId: sendId,
-  appId: appId
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECTID,
+  storageBucket:process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  messagingSenderId:process.env.EXPO_PUBLIC_SENDERID,
+  appId: process.env.EXPO_PUBLIC_APPID
 }
 
 export const FIREBASE_APP = initializeApp(firebaseConfig)
