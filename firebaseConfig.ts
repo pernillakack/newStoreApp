260f1aa329e74ee00573ea08391ad5ae9e6fd054
PhilNordin <112933450+PhilNordin@users.noreply.github.com>
2023-12-04 12:20:32 +0100

    config remap

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,14 +2,21 @@ import { initializeApp } from "firebase/app"
 // import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
+const key = process.env.EXPO_PUBLIC_FIREBASE_KEY
+const aDomain = process.env.EXPO_PUBLIC_AUTHDOMAIN
+const pId = process.env.EXPO_PUBLIC_PROJECTID
+const storageB = process.env.EXPO_PUBLIC_STORAGEBUCKET
+const sendId = process.env.EXPO_PUBLIC_SENDERID
+const appId = process.env.EXPO_PUBLIC_APPID
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnEesW48z8i42g49Y9wTx31GKSUecgmvY",
-  authDomain: "funstore-67017.firebaseapp.com",
-  projectId: "funstore-67017",
-  storageBucket: "funstore-67017.appspot.com",
-  messagingSenderId: "156467690288",
-  appId: "1:156467690288:web:a90acdf591959cf9ada6f5"
+  apiKey: key,
+  authDomain: aDomain,
+  projectId: pId,
+  storageBucket:storageB,
+  messagingSenderId: sendId,
+  appId: appId
 }
 
 export const FIREBASE_APP = initializeApp(firebaseConfig)
